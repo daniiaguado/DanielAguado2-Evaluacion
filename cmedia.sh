c9686c03accdf7cd52cf5d5f1dc1e87906bbbd14
Daniel Aguado <daagro@floridauniversitaria.es>
2024-02-20 10:33:29 +0100

    a

--- cmedia.sh
+++ cmedia.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Dime un sitio "
+echo "Dime un sitioo "
 
 read ciudad
 
 dias=`cat consumos.txt | grep $ciudad| wc -l`
 
 cat consumos.txt | grep $ciudad| awk '{suma += $4} END {print suma / $dias}'
+
 
