19375bc2ab1bf182611c2f4a12f3225069147158
Daniel Aguado <daagro@floridauniversitaria.es>
2024-02-20 10:31:32 +0100

    c

---
+++ cmedia.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Dime un sitio "
+
+read ciudad
+
+dias=`cat consumos.txt | grep $ciudad| wc -l`
+
+cat consumos.txt | grep $ciudad| awk '{suma += $4} END {print suma / $dias}'
+
