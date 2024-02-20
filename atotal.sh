32355b2ac8daf4417d6d87c9a3af1f19097d07ff
Daniel Aguado <daagro@floridauniversitaria.es>
2024-02-20 10:09:40 +0100

    areglar atotal error

---
+++ atotal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while true; do
+    read -p "introduce una de estas 3 ciudad Madrid , Valencia o Barcelona : " ciudad
+
+     if [[ "$ciudad" == "Madrid"  ]]; then
+        cat consumos.txt | grep $ciudad |  awk '{suma += $4 } END {print suma}'S
+    elif [[ "$ciudad" == "Valencia"  ]]; then
+        cat consumos.txt | grep $ciudad |  awk '{suma += $4 } END {print suma}'
+    elif  [[ "$ciudad" == "Barcelona"  ]]; then
+       cat consumos.txt | grep $ciudad |  awk '{suma += $4 } END {print suma}'
+       break
+        else 
+        echo "La ciudad introducida no es valida porfavor introduce una de estas 3 ciudad Madrid , Valencia o Barcelona"
+    fi
+done
+
+<<<<<<< HEAD
+
+
+=======
+>>>>>>> parte1
